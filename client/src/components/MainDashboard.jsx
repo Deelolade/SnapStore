8bb5ae64947748279929aabaa94cd66dd85e4793
Deelolade <oluwanisholahabeeb13@icloud.com>
2025-07-28 14:12:54 +0100

    created the dahsboard

--- client/src/components/MainDashboard.jsx
+++ client/src/components/MainDashboard.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Example from './DashboardGraph'
+import { useSelector } from 'react-redux'
+import DashboardProductLists from './DashboardProductLists'
 
 const MainDashboard = () => {
+    const  currentUser = useSelector((state) => state.user.user )
+    console.log(currentUser)
   return (
-    <div>MainDashboard</div>
+    <>
+        <main className=" w-[85%] px-8 h-screen py-10">
+        <h1 className='text-4xl font-semibold'>Welcome, {currentUser.name.split(" ").at(-1)} 👋</h1>
+        <Example/>
+        <DashboardProductLists/>
+        </main>
+    </>
   )
 }
 
