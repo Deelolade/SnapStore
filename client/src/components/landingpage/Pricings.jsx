7c61faa14ed63eb815317fb9fe4cd0dfe6ebc36d
Deelolade <oluwanisholahabeeb13@icloud.com>
2025-10-14 14:59:52 +0100

    modified the pricings section

--- client/src/components/landingpage/Pricings.jsx
+++ client/src/components/landingpage/Pricings.jsx
@@ -4,13 +4,13 @@ const Pricings = () => {
     return (
         <div>
             <section id="pricing" className="py-20 bg-gray-50 text-center">
-                <h2 className="text-4xl  mb-4">Simple, Transparent Pricing</h2>
-                <p className="text-lg mb-10">Start for free. Upgrade anytime as your business grows.</p>
-                <div className="grid grid-cols-2 max-w-6xl mx-auto rounded-3xl">
-                    <div className="rounded-l-3xl bg-white p-10 shadow-lg text-left">
-                        <h3 className="text-2xl mb-2">Starter Plan</h3>
-                        <p className="text-4xl font-semibold mb-4">Free</p>
-                        <p className="text-gray-600 mb-6">
+                <h2 className="text-2xl md:text-3xl lg:text-5xl font-semibold md:font-normal">Simple, Transparent Pricing</h2>
+                <p className="mt-2 md:mt-4 text-sm md:text-xl mb-10">Start for free. Upgrade anytime as your business grows.</p>
+                <div className="grid md:grid-cols-2 max-w-6xl mx-auto px-3  2xl:rounded-sm">
+                    <div className="rounded-l-sm bg-white p-5 2xl:p-10 shadow-lg text-left">
+                        <h3 className="text-lg 2xl:text-2xl mb-2">Starter Plan</h3>
+                        <p className="text-2xl 2xl:text-4xl font-semibold mb-4">Free</p>
+                        <p className="text-gray-600 mb-4 2xl:mb-6">
                             Perfect for small business owners and creators who just want to get started fast.
                             No credit card needed — just sign up, add your products, and share your store link.
                         </p>
@@ -28,12 +28,12 @@ const Pricings = () => {
                                 <IoCheckmarkDoneSharp className="text-2xl text-orange"/> No hidden fees, no hassle
                             </li>
                         </ul>
-                        <button className="bg-orange text-white px-6 py-3 rounded-sm font-semibold hover:bg-orange-600 transition">
+                        <button className="bg-orange text-white px-4 2xl:px-6 py-2 2xl:py-3 rounded-sm font-semibold transition">
                             Get Started Free
                         </button>
                     </div>
-                    <div className="">
-                        <img src={imageOne} className="h-[600px] object-cover rounded-r-3xl w-full" alt="" />
+                    <div className="hidden md:block">
+                        <img src={imageOne} className="h-[600px] object-cover rounded-r-sm w-full" alt="" />
                     </div>
                 </div>
             </section>
