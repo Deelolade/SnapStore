fde76cf247fedfec378f51c9d6f0eae247956e10
Deelolade <oluwanisholahabeeb13@icloud.com>
2025-10-14 22:22:34 +0100

    removed the sliding image on hero section for mobile

--- client/src/components/landingpage/HeroSection.jsx
+++ client/src/components/landingpage/HeroSection.jsx
@@ -16,8 +16,8 @@ const HeroSection = () => {
                 const tl = gsap.timeline({
                     scrollTrigger: {
                         trigger: heroImage.current,
-                        start: ' 100% 10%',
-                        end: ' 30% 90%',
+                        start: ' 100% 90%',
+                        end: ' 30% 10%',
                         scrub: true,
                         pin: heroText.current,
                         pinSpacing: false,
@@ -49,43 +49,43 @@ const HeroSection = () => {
                 })
 
             },
-            '(max-width:767px)': function () {
-                const tl = gsap.timeline({
-                    scrollTrigger: {
-                        trigger: heroImage.current,
-                        start: ' 100% 100%',
-                        end: ' 30% 10%',
-                        scrub: true,
-                        pin: heroText.current,
-                        pinSpacing: false,
-                    }
-                })
-                tl.to(heroImage.current, {
-                    scale: 1,
-                    ease: 'none'
-                }).to(buttonRef.current, {
-                    borderColor: "white",
-                    color: 'white',
-                    duration: 5,
-                }).to(heroText.current.querySelectorAll('p')[1], {
-                    color: 'white',
-                    duration: 5,
-                    ease: 'none'
-                }).to(heroText.current.querySelectorAll('p')[0], {
-                    color: 'white',
-                    duration: 3,
-                    ease: 'none'
-                }).to(heroText.current.querySelectorAll('h1')[1], {
-                    color: 'white',
-                    duration: 3,
-                    ease: 'none'
-                }).to(heroText.current.querySelectorAll('h1')[0], {
-                    color: 'white',
-                    duration: 3,
-                    ease: 'none'
-                })
+            // '(max-width:767px)': function () {
+            //     const tl = gsap.timeline({
+            //         scrollTrigger: {
+            //             trigger: heroImage.current,
+            //             start: ' 90% 100%',
+            //             end: ' 10% 10%',
+            //             scrub: true,
+            //             pin: heroText.current,
+            //             pinSpacing: false,
+            //         }
+            //     })
+            //     tl.to(heroImage.current, {
+            //         scale: 1,
+            //         ease: 'none'
+            //     }).to(buttonRef.current, {
+            //         borderColor: "white",
+            //         color: 'white',
+            //         duration: 5,
+            //     }).to(heroText.current.querySelectorAll('p')[1], {
+            //         color: 'white',
+            //         duration: 5,
+            //         ease: 'none'
+            //     }).to(heroText.current.querySelectorAll('p')[0], {
+            //         color: 'white',
+            //         duration: 3,
+            //         ease: 'none'
+            //     }).to(heroText.current.querySelectorAll('h1')[1], {
+            //         color: 'white',
+            //         duration: 3,
+            //         ease: 'none'
+            //     }).to(heroText.current.querySelectorAll('h1')[0], {
+            //         color: 'white',
+            //         duration: 3,
+            //         ease: 'none'
+            //     })
 
-            }
+            // }
         })
         // return () => tl.kill();
     })
@@ -95,31 +95,31 @@ const HeroSection = () => {
             <main>
                 <section className="min-h-screen flex justify-center items-center bg-white">
                     <div
-                        className="max-w-6xl h-[316px] text-center hero-text z-40 "
+                        className="max-w-6xl h-[316px] text-center hero-text z-40 p-3"
                         ref={heroText}
                     >
                         <h1 className="text-[30px] md:text-[55px] 2xl:text-[100px] font-semibold text-black text-center 2xl:leading-[110px]">
                             Share your products.
                         </h1>
-                        <h1 className="md:text-[60px] 2xl:text-[100px] font-semibold text-black text-center 2xl:leading-[110px]">Build your dream.</h1>
+                        <h1 className="text-[30px] md:text-[60px] 2xl:text-[100px] font-semibold text-black text-center 2xl:leading-[110px]">Build your dream.</h1>
                         <div className="md:mt-6 2xl:mt-10">
-                            <p className="text-black font-semibold text-lg text-center">
+                            <p className="text-black md:font-semibold text-sm md:text-lg text-center">
                                 You don't need a fancy website to start. Just upload what you sell,
                             </p>
-                            <p className="text-black font-semibold text-lg text-center">
+                            <p className="text-black md:font-semibold text-sm  md:text-lg text-center">
                                 and let people discover your brand one post at a time.
                             </p>
                         </div>
-                        <button ref={buttonRef} className=" text-lg text-black mt-4  font-semibold py-2 px-4 rounded-sm border-black border-2">Start Sharing Now</button>
+                        <button ref={buttonRef} className=" text-sm md:text-lg text-black mt-4  font-semibold py-2 px-4 rounded-sm border-black border-2">Start Sharing Now</button>
                     </div>
                 </section>
-                <section className="flex justify-center items-center ">
+                <section className="hidden md:flex justify-center items-center ">
                     <div className="max-w-fit">
                         <img
                             src={imageOne}
                             alt="image-one"
                             ref={heroImage}
-                            className="image-one w-screen md:h-[400px] 2xl:h-[500px] object-cover"
+                            className="image-one w-screen h-[350px] md:h-[400px] 2xl:h-[500px] object-cover"
                         />
                     </div>
                 </section>
