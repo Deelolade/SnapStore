92eefe04285f537b5924a8529670baf1089a2c4f
Deelolade <oluwanisholahabeeb13@icloud.com>
2025-10-14 15:13:26 +0100

    added footer responsiveness

--- client/src/components/landingpage/CTA.jsx
+++ client/src/components/landingpage/CTA.jsx
@@ -10,10 +10,10 @@ const CTA = () => {
                 <p className="text-sm md:text-lg mb-5 2xl:mb-10 max-w-3xl mx-auto">Join thousands of small business owners already showcasing and selling their products effortlessly.
                     No coding, no stress — just results.</p>
                 <div className="flex justify-center gap-4 flex-wrap">
-                    <button className="bg-white text-orange text-sm  font-semibold px-5 2xl:px-8 py-2 2xl:py-3 rounded-sm hover:bg-gray-100 transition duration-300">
+                    <button className="bg-white text-orange text-sm md:text-lg  font-semibold px-5 2xl:px-8 py-2 2xl:py-3 rounded-sm hover:bg-gray-100 transition duration-300">
                         Get Started Free
                     </button>
-                    <button className="border-2 border-white text-sm  text-white font-semibold px-5 2xl:px-8 py-2 2xl:py-3 rounded-sm transition duration-300">
+                    <button className="border-2 border-white text-sm md:text-lg text-white font-semibold px-5 2xl:px-8 py-2 2xl:py-3 rounded-sm transition duration-300">
                         See How It Works
                     </button>
                 </div>
--- client/src/components/landingpage/Footer.jsx
+++ client/src/components/landingpage/Footer.jsx
@@ -4,17 +4,16 @@ import { SiInstagram } from "react-icons/si";
 import { FaGithub } from "react-icons/fa6";
 const Footer = () => {
     const date = new Date()
-console.log(date.getFullYear())
     return (
         <>
-            <footer className='h-[10vh] bg-green shadow-2xl'>
-                <div className="h-[10vh] max-w-7xl text-white bg-green flex justify-between items-center mx-auto">
-                    <a href="#" className='text-2xl'>SnapStore</a>
-                    <p>Copyright {date.getFullYear()} </p>
-                    <div className=" flex justify-center items-center space-x-4">
-                        <a href="https://x.com/deelolade"><BsTwitterX className='text-2xl '/></a>
-                       <a href="https://www.instagram.com/deelolade"><SiInstagram className='text-2xl'/></a>
-                        <a href="https://github.com/deelolade"><FaGithub className='text-2xl'/></a>
+            <footer className=' bg-green shadow-2xl'>
+                <div className="h-[10vh] max-w-7xl px-3 md:px-5 text-white bg-green flex justify-between items-center mx-auto">
+                    <a href="#" className='md:text-2xl'>SnapStore</a>
+                    <p className='text-sm md:text-lg'>Copyright {date.getFullYear()} </p>
+                    <div className=" flex justify-center items-center space-x-2 md:space-x-4">
+                        <a href="https://x.com/deelolade"><BsTwitterX className='md:text-2xl '/></a>
+                       <a href="https://www.instagram.com/deelolade"><SiInstagram className='md:text-2xl'/></a>
+                        <a href="https://github.com/deelolade"><FaGithub className='md:text-2xl'/></a>
                     </div>
                 </div>
             </footer>
